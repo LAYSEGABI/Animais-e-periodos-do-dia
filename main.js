dd776a01e97e3e43ade91afce154dc801be2d103
LAYSEGABI <laysegabrielly13@gmail.com>
2023-10-05 10:10:13 -0300

    projeto concluido

---
+++ main.js
@@ -0,0 +1,48 @@
+let box = document.getElementById('box')
+let botaoDia = document.getElementById('butao-dia')
+let botaoTarde = document.getElementById('butao-tarde')
+let botaoNoite = document.getElementById('butao-noite')
+
+let botaoCachorro = document.getElementById('butao-cachorro')
+let botaoGato = document.getElementById('butao-gato')
+let botaoGalo = document.getElementById('butao-galo')
+
+let cachorroImg = document.getElementById('cachorro')
+let gatoImg = document.getElementById('gato')
+let galoImg = document.getElementById('galo')
+
+
+botaoDia.addEventListener('click', function () {
+    box.style.background = "url('céu.png')"
+})
+
+botaoTarde.addEventListener('click', function () {
+    box.style.background = "url('tarde.png')"
+})
+
+botaoNoite.addEventListener('click', function () {
+    box.style.background = "url('noite.png')"
+})
+
+
+botaoCachorro.addEventListener('click', function () {
+    cachorroImg.style.display = "block"
+    gatoImg.style.display = "none"
+    galoImg.style.display = "none"
+
+    console.log('teste')
+})
+
+botaoGato.addEventListener('click', function () {
+    gatoImg.style.display = "block"
+    cachorroImg.style.display = "none"
+    galoImg.style.display = "none"
+    console.log('teste')
+})
+
+botaoGalo.addEventListener('click', function () {
+    gatoImg.style.display = "none"
+    cachorroImg.style.display = "none"
+    galoImg.style.display = "block"
+    console.log('teste')
+})
